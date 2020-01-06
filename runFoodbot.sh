9648a1a2c447265144008818d74114203e54dc31
WViiras <viirsalu.villu@gmail.com>
2020-01-06 14:44:15 +0200

    updated shell script

--- runFoodbot.sh
+++ runFoodbot.sh
@@ -1,28 +1,26 @@
 #!/usr/bin/env bash
 
-script_root=~/my-script_root/path
-
-logfile=${HOME}/foodbot.log
-
 channel=bot_test
 #channel=tallinn-lunch
 
-cd $script_root
-
-run_daily() {
-    python3 foodbot_main.py -channel ${channel} >> "${logfile}"
+run_script() {
+    (cd ~/path/to/script/folder && python3 foodbot_main.py -channel ${channel} "${@}") #>> "${logfile}")
 }
 
-send_text_from_file(){
-    custom_path=$script_root/slack_foodbot/resources/custom.msg
-    python3 foodbot_main.py -channel ${channel} -custom_path "${custom_path}" #>> "${logfile}"
+run_daily() {
+    run_script
+}
+
+send_text_from_file() {
+    custom_path=~/path/to/some/file/custom.msg
+    run_script -custom_path "${custom_path}"
 }
 
 send_image() {
-    image_path=${script_root}/slack_foodbot/resources/test-cat.jpeg
-    python3 foodbot_main.py -channel ${channel} -image_path "${image_path}" #>> "${logfile}"
+    image_path=~/Desktop/test-cat.jpeg
+    run_script -image_path "${image_path}"
 }
 
 run_daily
-#run_send_image
-#run_send_custom
+#send_image
+#send_text_from_file
